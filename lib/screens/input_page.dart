1da886b206ffa7d207430447bab0f3b8a6d84adf
Juliana <juliana@itsencrypted.com>
2020-01-27 21:15:05 -0300

    app completed

--- lib/screens/input_page.dart
+++ lib/screens/input_page.dart
@@ -1,8 +1,13 @@
+import 'package:bmi_calculator/screens/results_page.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'icon_content.dart';
-import 'reusable_card.dart';
-import 'constants.dart';
+import '../components/icon_content.dart';
+import '../components/reusable_card.dart';
+import '../constants.dart';
+import 'results_page.dart';
+import '../components/botton_button.dart';
+import '../components/round_icon_button.dart';
+import 'package:bmi_calculator/calculator_brain.dart';
 
 enum Gender{male, female}
 
@@ -161,38 +166,21 @@ class _InputPageState extends State<InputPage> {
               ],
             ),
             ),
-            Container(
-              color: KBottomContainerColor,
-              margin: EdgeInsets.only(top: 10.0),
-              width: double.infinity,
-              height: kBottomContainerHeight,
-            ),
+            BottomButton(buttonTitle: 'CALCULATE', onTap: (){
+
+              CalculatorBrain calc = CalculatorBrain(height: height, weight: weight);
+
+              Navigator.push(context, MaterialPageRoute(builder: (context) => ResultsPage(
+                bmiResult: calc.calculateBMI(),
+                resultText: calc.getResult(),
+                interpretation: calc.getInterpretation(),
+              )));
+            },),
           ],));
   }
 }
 
 
-class RoundIconButton extends StatelessWidget {
-
-  RoundIconButton({@required this.icon, @ required this.onPressed });
-  final IconData icon;
-  final Function onPressed;
-
-  @override
-  Widget build(BuildContext context) {
-    return RawMaterialButton(
-      child: Icon(icon) ,
-      onPressed: onPressed,
-      elevation: 6.0,
-      constraints: BoxConstraints.tightFor(
-        width: 56.0,
-        height: 56.0,
-      ),
-      shape: CircleBorder(),
-      fillColor: Color(0xFF4C4F5E),
-    );
-  }
-}
 
 
 
